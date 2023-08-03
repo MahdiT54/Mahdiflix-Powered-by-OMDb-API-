5818237e930456fb52b5119529c91ad2c7633c40
Mahdi <mahditanzim2@gmail.com>
2023-08-03 03:56:25 -0400

    new layout

---
+++ index.js
@@ -0,0 +1,68 @@
+// Titles: http://www.omdbapi.com/?s=thor&page=1&apikey=1c44ead
+// details: http://www.omdbapi.com/?i=tt3896198&apikey=1c44ead
+const movieListEl = document.querySelector(".movie-list");
+const headerContainer = document.querySelector(".header__image--container")
+const movieSearchBox = document.getElementById("search__list");
+
+async function loadMovies(searchTerm) {
+  console.log(searchTerm);
+  const URL = `https://www.omdbapi.com/?s=${searchTerm}&page=1&apikey=1c44ead`; // dynamic
+  // Show loading state
+  headerContainer.innerHTML = ''
+  movieListEl.innerHTML =
+    "<div class='loading'><i class='fas fa-spinner'></div>";
+  // Simulate a 1-second delay using setTimeout
+  setTimeout(async () => {
+    try {
+      const res = await fetch(URL);
+      const data = await res.json();
+      if (!data.Search || data.Search.length === 0) {
+        // If no movies are found, display the message
+        movieListEl.innerHTML =
+          '<div class="no-results">No movies available by that search term.</div>';
+        return; // exit
+      }
+      let sixMovies = data.Search.slice(0, 6); // Keep only the first 6 movies
+      movieListEl.innerHTML = sixMovies
+        .map((movie) => movieHTML(movie))
+        .join("");
+      console.log(sixMovies);
+    } catch (error) {
+      // Handle errors here if needed
+      console.error(error);
+      movieListEl.innerHTML = '<div class="error">Error loading movies.</div>';
+    }
+  }, 1000); // 1000 milliseconds = 1 second
+}
+
+function findMovies() {
+  let searchTerm = movieSearchBox.value.trim();
+  loadMovies(searchTerm);
+}
+
+function showMovieInfo(id) {
+  localStorage.setItem("imdbID", id);
+  window.location.href = `${window.location.origin}/moviepage.html`;
+}
+
+function movieHTML(movie) {
+  return `
+    <div class="movie-card" onclick="showMovieInfo('${movie.imdbID}')">
+        <div class="movie-card__container">
+            <div class="movie-poster">
+                <div class="search-item-thumbnail">
+                    <img src="${
+                      movie.Poster !== "N/A"
+                        ? movie.Poster
+                        : "./assets/image_not_found.png"
+                    }" class="search-item-img" alt="movie poster" />
+                </div>
+                <div class="search-item-info">
+                    <h3 class="search-item-h3">${movie.Title}</h3>
+                    <p class="search-item-p">${movie.Year}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+}
---
+++ moviepage.js
@@ -0,0 +1,43 @@
+const postListEl = document.querySelector(".movie-list");
+const id = localStorage.getItem("imdbID");
+
+async function renderPosts() {
+  const post = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=1c44ead`);
+  const postData = await post.json();
+  console.log(postData);
+  postListEl.innerHTML = postHTML(postData); // Set the HTML directly, as it's a single movie
+}
+
+function postHTML(post) {
+  return `
+  <div class="movie-poster">
+  <img src="${
+    post.Poster !== "N/A" ? post.Poster : "./assets/image_not_found.png"
+  }" alt="movie poster" />
+  </div>
+  <div class="movie-info">
+      <h3 class="movie-title">${post.Title}</h3>
+      <ul class="movie-misc-info">
+          <li class="year">Year: ${post.Year}</li>
+          <li class="rated">Ratings: ${post.Rated}</li>
+          <li class="released">Released: ${post.Released}</li>
+      </ul>
+      <p class="genre"><b>Genre:</b> ${post.Genre}</p>
+      <p class="writer">
+      <b>Writer:</b> ${post.Writer}
+      </p>
+      <p class="actors">
+      <b>Actors:</b> ${post.Actors}
+      </p>
+      <p class="plot">
+      <b>Plot:</b> ${post.Plot}
+      </p>
+      <p class="language"><b>Language:</b> ${post.Language}</p>
+      <p class="awards">
+      <b> <i class="fas fa-award"></i></b> ${post.Awards}
+      </p>
+  </div>
+    `;
+}
+
+renderPosts();
